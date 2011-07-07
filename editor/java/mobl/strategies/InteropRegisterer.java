69f57a7d579c719c2a75c4523d6ce9bc37a1eb5b
Zef Hemel <zef@zef.me>
2011-07-07 16:51:32 +0200

    Disabling uglify

--- editor/java/mobl/strategies/InteropRegisterer.java
+++ editor/java/mobl/strategies/InteropRegisterer.java
@@ -9,7 +9,7 @@ import org.strategoxt.lang.Strategy;
 public class InteropRegisterer extends JavaInteropRegisterer {
 
     public InteropRegisterer() {
-        super(new Strategy[] { deltree_0_0.instance, uglify_0_0.instance, compress_0_0.instance,
+        super(new Strategy[] { deltree_0_0.instance,/* uglify_0_0.instance,*/ compress_0_0.instance,
                 path_to_dataurl_0_0.instance });
     }
 }
